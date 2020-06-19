ccc9e28f40ee5499299abf2ab5126feab1820ed9
t0903 <1051836830@qq.com>
2020-06-19 12:33:34 +0800

    1. 学生信息的添加

--- StudentController.class.php
+++ StudentController.class.php
@@ -18,6 +18,29 @@ final class StudentController{
         include 'StudentIndexView.html';
     }
 
+    public function add(){
+        include 'StudentAddView.html';
+    }
+
+    public function insert(){
+        $data['no'] = $_POST['no'];
+        $data['name'] = $_POST['name'];
+        $data['sex'] = $_POST['sex'];
+        $data['birthday'] = strtotime($_POST['birth']);
+        $data['major'] = $_POST['major'];
+        $data['phone'] = $_POST['phone'];
+
+        if($this -> model -> insert($data)){
+            echo "<h3>记录添加成功！</h3>";
+            header("refresh:3;url=?");
+            exit();
+        }else{
+            echo "<h3>记录添加失败！</h3>";
+            header("refresh:3;url=?");
+            exit();
+        }
+    }
+
     public function delete(){
         $id = $_GET['id'];
         if($this -> model -> delete($id)){
@@ -40,4 +63,8 @@ if($ac == 'index'){
     $controller -> index();
 }elseif($ac == 'delete'){
     $controller -> delete();
+}elseif($ac == 'add'){
+    $controller -> add();
+}elseif($ac == 'insert'){
+    $controller -> insert();
 }
--- StudentModel.class.php
+++ StudentModel.class.php
@@ -13,4 +13,19 @@ class StudentModel extends BaseModel
         $sql = "delete from student where id={$id}";
         return $this -> db -> exec($sql);
     }
+
+    public function insert($data){
+        $fields = '';
+        $values = '';
+        foreach ($data as $k => $v){
+            $fields .= "$k,";
+            $values .= "'$v',";
+        }
+        $fields = rtrim($fields,',');
+        $values = rtrim($values,',');
+
+        $sql = "insert into student($fields) values($values)";
+
+        return $this -> db ->exec($sql);
+    }
 }
